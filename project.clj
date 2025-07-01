8fe528d86d766ceb0ef85dbfd65a526ffaeb7408
Gabriel Rech <gabriel.rech@nubank.com.br>
2025-07-01 14:00:21 -0300

    fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.gaelrech/tapp "0.0.8"
+(defproject org.clojars.gaelrech/tapp "0.0.9"
   :description "Debugging tools for general usage"
   :url "https://github.com/gaelrech/tapp"
   :license {:name "The MIT License"
